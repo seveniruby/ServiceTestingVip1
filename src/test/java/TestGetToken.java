4d5fe5a6dabcae8e7da2be37d6c0a2c3873fb82b
seveniruby <seveniruby@gmail.com>
2019-03-17 10:31:33 +0800

    单例配置

--- src/test/java/TestGetToken.java
+++ src/test/java/TestGetToken.java
@@ -7,8 +7,8 @@ public class TestGetToken {
     @Test
     void testToken(){
         RestAssured.given().log().all()
-                .queryParam("corpid", "wwd6da61649bd66fea")
-                .queryParam("corpsecret", "1JPyY9GvPLZfpvxEDjok-Xt_9v7HIBYJhZUoO6EgNGY")
+                .queryParam("corpid", WeworkConfig.getInstance().corpid)
+                .queryParam("corpsecret", WeworkConfig.getInstance().secret)
             .when().get("https://qyapi.weixin.qq.com/cgi-bin/gettoken")
             .then().log().all().statusCode(200).body("errcode", equalTo(0));
     }
---
+++ src/test/java/WeworkConfig.java
@@ -0,0 +1,15 @@
+public class WeworkConfig {
+
+    public String agentId="1000005";
+    public String secret="1JPyY9GvPLZfpvxEDjok-Xt_9v7HIBYJhZUoO6EgNGY";
+    public String corpid = "wwd6da61649bd66fea";
+    public String contactSecret="C7uGOrNyxWWzwBsUyWEbLQdOqoWPz4hNvxj9RIFv-4U";
+
+    private static WeworkConfig weworkConfig;
+    public static WeworkConfig getInstance(){
+        if(weworkConfig==null){
+            weworkConfig=new WeworkConfig();
+        }
+        return weworkConfig;
+    }
+}
