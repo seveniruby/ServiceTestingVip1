d0fd05c0fa2c0f73100e8acbd73db58731ec6f6d
seveniruby <seveniruby@gmail.com>
2019-03-17 18:12:33 +0800

    update

--- src/test/java/GetTokenTest.java
+++ src/test/java/GetTokenTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 
-public class TestGetToken {
+public class GetTokenTest {
     @Test
     void testToken(){
         Wework wework=new Wework();
